277d0a09efcd02c966ad65f4d2594918bbdf3bc6
Yuma Sumi <yuma.sumi@woven-planet.global>
2023-03-13 15:54:32 +0900

    added -ja files

---
+++ 07_variables-and-types/07_02_naming_errors/07_02_naming_errors-ja.py
@@ -0,0 +1,9 @@
+# 以下のコードを実行すると、SyntaxErrorが発生します。
+# 変数名がルールに準拠していません。
+# コードが正常に動作するように変数名を修正してください。
+
+1stvariable = 40
+second variable = 2
+
+finally = 1stvariable + second variable
+print(finally)
---
+++ projects/mazemaker/mazemaker-ja.py
@@ -0,0 +1,3 @@
+# 以下のチュートリアルに従って迷路ジェネレーターを構築し、
+# 外部リソースを利用した学習に取り組んでみましょう。
+# https://medium.com/swlh/fun-with-python-1-maze-generator-931639b4fb7e
